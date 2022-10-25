89b087680292c009b523cbc2a22bf9d213ec6f2c
potipot <pawel.potrykus@hayden.ai>
2022-10-25 19:56:30 +0200

    add resolve path

--- pytrt/run.py
+++ pytrt/run.py
@@ -10,7 +10,7 @@ from viz import draw_mask
 def main(verbose: bool = False, workspace: int = 2, precision: str = "fp16"):
     """Create a TensorRT engine for ONNX-based YOLOv3-608 and run inference."""
 
-    path = Path(__file__).parent.parent
+    path = Path(__file__).parent.parent.resolve()
     encoder_onnx_filepath = path / "models/onnx/dual/encoder.onnx"
     decoders_onnx_filepath = path / "models/onnx/dual/decoders.onnx"
     encoder_engine_filepath = path / f"models/trt/dual/{encoder_onnx_filepath.stem}.engine"
@@ -34,11 +34,11 @@ def main(verbose: bool = False, workspace: int = 2, precision: str = "fp16"):
     for image_path in images_dir.iterdir():
         image_raw, image_preprocessed = processor.process(image_path)
         encoder_output = encoder_inference.infer(image_preprocessed)
-        logger.debug(f"Encoder inference time: {encoder_inference.inference_time}")
+        logger.debug(f"Encoder inference time: {encoder_inference.inference_time} ms")
         feature_map, *_ = encoder_output
         decoders_output = decoders_inference.infer(feature_map)
-        logger.debug(f"Decoders inference time: {decoders_inference.inference_time}")
-        logger.info(f"Total inference time: {encoder_inference.inference_time+decoders_inference.inference_time}")
+        logger.debug(f"Decoders inference time: {decoders_inference.inference_time} ms")
+        logger.info(f"Total inference time: {encoder_inference.inference_time+decoders_inference.inference_time} ms")
         mask = decoders_output[-1]
         draw_mask(mask, image_path.name)
 
